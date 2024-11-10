78438d5d9e9dfb231a0e00934815c2a2f517013d
horanhe <13571899655@163.com>
2024-11-10 23:26:09 +0800

    fix: upgrade http sdk

--- pkg/http/http.go
+++ pkg/http/http.go
@@ -4,132 +4,181 @@ import (
 	"bytes"
 	"context"
 	"encoding/json"
+	"errors"
 	"github.com/gocarina/gocsv"
 	"io"
 	"microservices/pkg/consts"
 	"microservices/pkg/log"
 	"net/http"
 	"net/url"
+	"strings"
 	"time"
 )
 
-type HttpRequest[T any] struct {
-	client  *http.Client
-	timeout int
+type Client interface {
+	Post(ctx context.Context, path string, urlParams map[string]string, reqBody any, respBody any, opts ...Option) error
+	Get(ctx context.Context, path string, urlParams map[string]string, respBody any, opts ...Option) error
+	Put(ctx context.Context, path string, urlParams map[string]string, reqBody any, respBody any, opts ...Option) error
+	Delete(ctx context.Context, path string, urlParams map[string]string, reqBody any, respBody any,
+		opts ...Option) error
+	Patch(ctx context.Context, path string, urlParams map[string]string, reqBody any, respBody any,
+		opts ...Option) error
 }
 
-func (h *HttpRequest[T]) Get(ctx context.Context, path string, urlParams map[string]string, body map[string]any,
-	headers map[string]string) (*T, error) {
-	return h.request(ctx, path, http.MethodGet, urlParams, body, headers)
+type client struct {
+	client *http.Client
+	opts   []Option
 }
 
-func (h *HttpRequest[T]) GetRawResponse(ctx context.Context, path string, urlParams map[string]string, body map[string]any,
-	headers map[string]string) ([]byte, error) {
-	response, err := h.sendRequest(ctx, path, http.MethodGet, urlParams, body, headers)
+// 创建 一个 http 客户端，通过传入参数 WithBaseUrl 来设置基础的 url
+func NewClient(opts ...Option) Client {
+	c := &client{}
+	c.opts = make([]Option, 0, len(opts)+1)
+	c.opts = append(c.opts, WithProtocol("http"), WithJsonRequest(), WithJsonResponse(), WithTimeout(time.Second*5))
+	c.opts = append(c.opts, opts...)
+	c.client = &http.Client{}
+	return c
+}
+
+// form/data 上传文件示例
+//
+//	 reqBody := &bytes.Buffer{}
+//		writer := multipart.NewWriter(reqBody)
+//		part, err := writer.CreateFormFile("file", filepath.Base(filePath))
+//		if err != nil {
+//			return err
+//		}
+//		_, err = io.Copy(part, file)
+//		if err != nil {
+//			return err
+//		}
+//		writer.Close()
+//		var resp any
+//
+// 需要注意使用 http.WithFormDataRequest(writer.FormDataContentType()) 来附加请求头 content-type 和 boundary
+//
+//	if err := s.client.Post(ctx, "", nil, reqBody, &resp, http.WithTimeout(time.Second*60), http.WithFormDataRequest(writer.FormDataContentType())); err != nil {
+//		return err
+//	}
+func (c *client) Post(ctx context.Context, path string, urlParams map[string]string, reqBody any, respBody any, opts ...Option) error {
+	return c.send(ctx, path, http.MethodPost, urlParams, reqBody, respBody, opts...)
+}
+
+func (c *client) Get(ctx context.Context, path string, urlParams map[string]string, respBody any, opts ...Option) error {
+	return c.send(ctx, path, http.MethodGet, urlParams, nil, respBody, opts...)
+}
+
+func (c *client) Put(ctx context.Context, path string, urlParams map[string]string, reqBody any, respBody any, opts ...Option) error {
+	return c.send(ctx, path, http.MethodPut, urlParams, reqBody, respBody, opts...)
+}
+
+func (c *client) Delete(ctx context.Context, path string, urlParams map[string]string, reqBody any, respBody any,
+	opts ...Option) error {
+	return c.send(ctx, path, http.MethodDelete, urlParams, reqBody, respBody, opts...)
+}
+
+func (c *client) Patch(ctx context.Context, path string, urlParams map[string]string, reqBody any, respBody any,
+	opts ...Option) error {
+	return c.send(ctx, path, http.MethodPatch, urlParams, reqBody, respBody, opts...)
+}
+func (c *client) send(ctx context.Context, path string, method string, urlParams map[string]string, reqBody any, respBody any, opts ...Option) error {
+	options, err := c.getOptions(opts)
 	if err != nil {
-		return nil, err
+		return err
 	}
-	return io.ReadAll(response.Body)
-}
-
-func (h *HttpRequest[T]) Post(ctx context.Context, path string, urlParams map[string]string, body map[string]any,
-	headers map[string]string) (*T, error) {
-	return h.request(ctx, path, http.MethodPost, urlParams, body, headers)
-}
-
-func (h *HttpRequest[T]) Put(ctx context.Context, path string, urlParams map[string]string, body map[string]any,
-	headers map[string]string) (*T, error) {
-	return h.request(ctx, path, http.MethodPut, urlParams, body, headers)
-}
-
-func (h *HttpRequest[T]) Delete(ctx context.Context, path string, urlParams map[string]string, body map[string]any,
-	headers map[string]string) (*T, error) {
-	return h.request(ctx, path, http.MethodDelete, urlParams, body, headers)
-}
-
-func (h *HttpRequest[T]) Patch(ctx context.Context, path string, urlParams map[string]string, body map[string]any,
-	headers map[string]string) (*T, error) {
-	return h.request(ctx, path, http.MethodPatch, urlParams, body, headers)
-}
-
-func (h *HttpRequest[T]) request(ctx context.Context, urlPath, method string, urlParams map[string]string, reqBody map[string]any,
-	headers map[string]string) (*T, error) {
-	response, err := h.sendRequest(ctx, urlPath, method, urlParams, reqBody, headers)
-	if err != nil {
-		return nil, err
-	}
-	contentType := response.Header.Get("content-type")
-	if err != nil {
-		return nil, err
-	}
-	defer response.Body.Close()
-	responseBody, err := io.ReadAll(response.Body)
-	if err != nil {
-		return nil, err
-	}
-	var res *T
-	if contentType == "text/csv" {
-		if err := gocsv.UnmarshalBytes(responseBody, &res); err != nil {
-			return nil, err
-		}
-	} else {
-		if err := json.Unmarshal(responseBody, &res); err != nil {
-			return nil, err
-		}
-	}
-	return res, nil
-}
-
-func (h *HttpRequest[T]) sendRequest(ctx context.Context, urlPath, method string, urlParams map[string]string, reqBody map[string]any,
-	headers map[string]string) (*http.Response, error) {
+	fullUrl := options.getBaseUrl() + path
 	if urlParams != nil {
 		values := url.Values{}
 		for k, v := range urlParams {
 			values.Add(k, v)
 		}
-		urlPath = urlPath + "?" + values.Encode()
+		fullUrl = fullUrl + "?" + values.Encode()
 	}
-	var reqBodyBuff []byte
+	var body io.Reader
 	if reqBody != nil {
-		var err error
-		reqBodyBuff, err = json.Marshal(reqBody)
-		if err != nil {
-			return nil, err
+		if options.RequestHeader.Get(ContentType) == ContentTypeApplicationJson {
+			reqBody, err := json.Marshal(reqBody)
+			if err != nil {
+				return err
+			}
+			body = bytes.NewReader(reqBody)
+		} else if strings.Contains(options.RequestHeader.Get(ContentType), ContentTypeFormData) {
+			body = reqBody.(*bytes.Buffer)
 		}
 	}
-	req, err := http.NewRequestWithContext(ctx, method, urlPath, bytes.NewReader(reqBodyBuff))
+	req, err := http.NewRequestWithContext(ctx, method, fullUrl, body)
 	if err != nil {
-		return nil, err
+		return err
 	}
-	for k, v := range headers {
-		req.Header.Add(k, v)
-	}
-	response, err := h.client.Do(req)
+	req.Header = options.RequestHeader
+	var responseReturn *string
+	resp, err := c.client.Do(req)
+	defer func() {
+		var httpStatus *int
+		if err == nil {
+			resp.Body.Close()
+			httpStatus = &resp.StatusCode
+		}
+		log.Info(ctx, "http-request", map[string]any{
+			"url":            fullUrl,
+			"reqBody":        reqBody,
+			"method":         method,
+			"responseStatus": httpStatus,
+			"responseBody":   responseReturn,
+		})
+	}()
 	if err != nil {
-		return nil, err
+		return err
 	}
-	responseBody, err := io.ReadAll(response.Body)
-	response.Body = io.NopCloser(bytes.NewBuffer(responseBody))
+	responseBody, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return err
+	}
 	responseStr := string(responseBody)
-	if len(responseBody) > consts.MaxResponseLogLength {
+	if len(responseStr) > consts.MaxResponseLogLength {
 		responseStr = responseStr[:consts.MaxResponseLogLength] + "..."
 	}
-	log.Info(ctx, "http-request", map[string]any{
-		"url":            urlPath,
-		"params":         urlParams,
-		"reqBody":        reqBody,
-		"method":         method,
-		"responseStatus": response.StatusCode,
-		"responseBody":   string(responseBody),
-	})
-	return response, err
+	responseReturn = &responseStr
+	//if resp.StatusCode != http.StatusOK {
+	//	return ecode.ErrHttpRequestError
+	//}
+	contentType := resp.Header.Get(ContentType)
+	if options.ResponseHeader.Get(contentType) == ContentTypeApplicationJson || contentType == ContentTypeApplicationJson {
+		if err := json.Unmarshal(responseBody, respBody); err != nil {
+			return err
+		}
+	} else if options.ResponseHeader.Get(contentType) == "text/csv" || contentType == "text/csv" {
+		if err := gocsv.UnmarshalBytes(responseBody, respBody); err != nil {
+			return err
+		}
+	} else {
+		respBody = responseBody
+	}
+	return nil
 }
 
-func NewHttp[T any](opts *Options) *HttpRequest[T] {
-	return &HttpRequest[T]{
-		client: &http.Client{
-			Timeout: time.Duration(opts.Timeout) * time.Second,
-		},
-		timeout: opts.Timeout,
+// getOptions returns Options
+func (c *client) getOptions(opts []Option) (*Options, error) {
+	options := &Options{}
+	// 附加全局参数
+	for _, opt := range c.opts {
+		opt(options)
 	}
+	// 附加每个单独请求的参数
+	for _, opt := range opts {
+		opt(options)
+	}
+	if options.Timeout > 0 {
+		c.client.Timeout = options.Timeout
+	}
+	if err := options.parseURL(); err != nil {
+		return nil, err
+	}
+	if options.Protocol == "" {
+		return nil, errors.New("protocol is empty")
+	}
+	if options.Domain == "" && options.IP == "" {
+		return nil, errors.New("domain and ip is empty")
+	}
+	return options, nil
 }
--- pkg/http/options.go
+++ pkg/http/options.go
@@ -1,11 +1,136 @@
 package http
 
+import (
+	"fmt"
+	"net/http"
+	"net/url"
+	"strconv"
+	"strings"
+	"time"
+)
+
+const ContentType = "Content-Type"
+const ContentTypeApplicationJson = "application/json"
+const ContentTypeFormData = "multipart/form-data"
+
+// Option sets client options.
+type Option func(*Options)
+
 type Options struct {
-	Timeout int
+	IP             string
+	Domain         string
+	Timeout        time.Duration
+	Protocol       string
+	ResponseHeader http.Header
+	RequestHeader  http.Header
+	Port           int
+	BaseUrl        string
+	PrefixUri      string
 }
 
-func NewOptions(timeout int) *Options {
-    return &Options{
-        Timeout: timeout,
-    }
+func (o *Options) getBaseUrl() string {
+	target := o.Domain
+	if target == "" {
+		target = o.IP
+	}
+	baseUrl := fmt.Sprintf("%s://%s", o.Protocol, target)
+	if o.Port != 0 {
+		return fmt.Sprintf("%s:%d", baseUrl, o.Port)
+	}
+	if o.PrefixUri != "" {
+		return fmt.Sprintf("%s/%s", baseUrl, strings.Trim(o.PrefixUri, "/"))
+	}
+	return baseUrl
+}
+
+func (o *Options) parseURL() error {
+	if o.BaseUrl != "" {
+		parsedUrl, err := url.Parse(o.BaseUrl)
+		if err != nil {
+			return err
+		}
+		o.Protocol = parsedUrl.Scheme // 协议
+		if strings.Contains(parsedUrl.Host, ":") {
+			parts := strings.Split(parsedUrl.Host, ":")
+			o.Domain = parts[0] // 域名
+		}
+		o.Port, err = strconv.Atoi(parsedUrl.Port()) // 端口，如果未指定则返回空字符串
+		if err != nil {
+			return err
+		}
+		o.PrefixUri = parsedUrl.Path // 路径
+	}
+	return nil
+}
+
+func WithTimeout(timeout time.Duration) Option {
+	return func(o *Options) {
+		o.Timeout = timeout
+	}
+}
+
+func WithDomain(domain string) Option {
+	return func(o *Options) {
+		o.Domain = domain
+	}
+}
+
+func WithProtocol(Protocol string) Option {
+	return func(o *Options) {
+		o.Protocol = Protocol
+	}
+}
+
+func WithIP(ip string) Option {
+	return func(o *Options) {
+		o.IP = ip
+	}
+}
+
+// WithBaseUrl 识别 http://127.0.0.1:8080 格式的 url，解析到 protocol， ip 或 domain 和 port 中
+func WithBaseUrl(baseUrl string) Option {
+	return func(o *Options) {
+		o.BaseUrl = baseUrl
+	}
+}
+
+func WithRequestHeader(header http.Header) Option {
+	return func(o *Options) {
+		o.RequestHeader = header
+	}
+}
+
+func WithJsonResponse() Option {
+	return func(o *Options) {
+		if o.ResponseHeader == nil {
+			o.ResponseHeader = make(http.Header)
+		}
+		o.ResponseHeader.Set(ContentType, ContentTypeApplicationJson)
+	}
+}
+
+func WithRawResponse() Option {
+	return func(o *Options) {
+		if o.ResponseHeader == nil {
+			o.ResponseHeader = make(http.Header)
+		}
+		o.ResponseHeader.Set(ContentType, "application/octet-stream")
+	}
+}
+
+func WithJsonRequest() Option {
+	return func(o *Options) {
+		if o.RequestHeader == nil {
+			o.RequestHeader = make(http.Header)
+		}
+		o.RequestHeader.Set(ContentType, ContentTypeApplicationJson)
+	}
+}
+func WithFormDataRequest(contentTypeWithBoundary string) Option {
+	return func(o *Options) {
+		if o.RequestHeader == nil {
+			o.RequestHeader = make(http.Header)
+		}
+		o.RequestHeader.Set(ContentType, contentTypeWithBoundary)
+	}
 }
