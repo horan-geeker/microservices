7e235d8e6844bd24d6cd6aa7bf711a32b3191cb8
horan <13571899655@163.com>
2025-12-26 23:48:30 +0800

    fix: auth

--- router/aigc.go
+++ router/aigc.go
@@ -4,11 +4,12 @@ import (
 	"microservices/cache"
 	"microservices/controller"
 	"microservices/model"
+	"microservices/router/middleware"
 	"microservices/service"
 )
 
 func init() {
 	aigcController := controller.NewAIGCController(model.NewFactory(), cache.NewFactory(), service.NewFactory())
-	router.POST("/aigc/generate", aigcController.Generate)
-	router.GET("/aigc/result", aigcController.Result)
+	router.POST("/aigc/generate", middleware.Authenticate(), aigcController.Generate)
+	router.GET("/aigc/result", middleware.Authenticate(), aigcController.Result)
 }
